998052a082e981d51ef9491816948f3871e7ea4c
Samuli Hakoniemi <samuli@hakoniemi.net>
2013-11-22 16:09:12 +0200

    Initial Commit

---
+++ Servant.js
@@ -0,0 +1,92 @@
+var Servant = (function() {
+    "use strict";
+
+    var elems = document.querySelectorAll("input, textarea");
+
+    var onelemfocus, onelemblur;
+
+    var isKeyboardShown = false;
+    var keyboardShowing, keyboardHiding;
+    var fieldBlur = null;
+    var currentInnerHeight;
+    var currIndex = 0, prevIndex = 0;
+    var isAClick = false;
+
+    onelemfocus = function(evt) {
+        var that = this;
+
+        prevIndex = currIndex;
+        currIndex = evt.target.dataset.index;
+
+        if (!isKeyboardShown) {
+            currentInnerHeight = window.innerHeight;
+            keyboardShowing = window.setInterval(function() {
+                if (currentInnerHeight === window.innerHeight) {
+                    clearInterval(keyboardShowing);
+
+                    var evtAfter = document.createEvent("Event");
+                    evtAfter.initEvent('keyboardshow', true, true);
+                    that.dispatchEvent(evtAfter);
+                    isKeyboardShown = true;
+                }
+                currentInnerHeight = window.innerHeight;
+            }, 20);
+        } else {
+            window.clearTimeout(fieldBlur);
+        }
+
+        if (!isAClick) {
+            var evtName;
+            
+            if (prevIndex < currIndex) {
+                evtName = "nextbuttonclick";
+            } else {
+                evtName = "previousbuttonclick";
+            }
+
+            var evtPrevNext = document.createEvent("Event");
+            evtPrevNext.initEvent(evtName, true, true);
+            that.dispatchEvent(evtPrevNext);
+        }
+    };
+
+    onelemblur = function(evt) {
+        var that = this;
+
+        fieldBlur = window.setTimeout(function() {
+            var evtBefore = document.createEvent("Event");
+            evtBefore.initEvent('keyboardhide', true, true);
+            that.dispatchEvent(evtBefore);
+            isKeyboardShown = false;
+        },1);
+    };
+
+    [].forEach.call(elems, function(elem, i) {
+        elem.dataset.index = i;
+        elem.addEventListener("focus", onelemfocus, false);
+        elem.addEventListener("blur", onelemblur, false);
+    });
+
+    // Hide keyboard when clicking document
+    var pageXO, pageYO;
+    document.documentElement.addEventListener("touchstart", function(evt) {
+        // Prevent keyboardhide when scrolling while keyboard is visible
+        pageXO = window.pageXOffset;
+        pageYO = window.pageYOffset;
+        isAClick = true;
+        window.setTimeout(function() {
+            isAClick = false;
+        }, 1000);
+    })
+    document.documentElement.addEventListener("touchend", function(evt) {
+        if (pageXO !== window.pageXOffset || pageYO !== window.pageYOffset) {
+            return false;
+        }
+        
+        var nodeName = evt.target.nodeName.toLowerCase();
+
+        if (nodeName !== "input" && nodeName !== "textarea" && nodeName !== "select") {
+            document.activeElement.blur();
+        }
+    });
+}());
